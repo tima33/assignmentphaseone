7e5d2c462a7dd8d3762f41cc1c0ae8a7979efdaa
tima33 <timothy.hayes@griffithuni.edu.au>
2024-09-17 17:14:32 +1000

    install initial components

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <h1>G'day from {{ name }}!</h1>
   `,
 })
 export class App {
